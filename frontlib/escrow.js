120b1a604c44d664f3b059a8b25ef91508aba5c7
OPReturnCode <mask.em@protonmail.com>
2020-09-05 17:42:20 +0430

    add contractCreationAcceptanceFormHandler to frontlib

--- frontlib/escrow.js
+++ frontlib/escrow.js
@@ -1,2 +1,27 @@
 const CryptoJS = require('crypto-js')
+const jeton = require('jeton-lib')
 
+
+function contractCreationAcceptanceFormHandler(formElementId, pubKeyElementId, privKeyElementId, token) {
+    const form = document.getElementById(formElementId)
+    const pubKeyElement =  document.getElementById(pubKeyElementId)
+    const privKeyElement =  document.getElementById(privKeyElementId)
+
+    function submitEventHandler(event) {
+        event.preventDefault()
+        pkey = jeton.PrivateKey()
+        pubKeyElement.setAttribute(
+            'value',
+            pkey.toPublicKey().toString('hex')
+            )
+        privKeyElement.setAttribute(
+            'value',
+            CryptoJS.AES.encrypt(pkey.toWIF(), token).toString()
+            )
+
+        form.submit()
+    }
+    form.addEventListener('submit', submitEventHandler)
+}
+
+window.contractCreationAcceptanceFormHandler = contractCreationAcceptanceFormHandler
